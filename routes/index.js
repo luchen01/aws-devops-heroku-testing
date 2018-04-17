76acd835955723c777b8d87a8206f3abc21a20b5
luchen01 <luchenpengnyc@gmail.com>
2018-04-17 16:44:54 -0700

    change title

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require("express")
 var router = express.Router()
 
 router.get("/", function(req, res, next) {
-	res.render("index", { title: "Node.js on Heroku" })
+	res.render("index", { title: "New fantastic app" })
 })
 
 module.exports = router
